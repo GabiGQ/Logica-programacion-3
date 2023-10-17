4f2fb79a7f0d756ae732a918476e65e1b51ab0bc
GabiGQ <gigutierrezquezada@gmail.com>
2023-10-17 15:50:43 -0300

    Blabla2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let num = parseFloat(prompt=()
+
+function factorial(){
+    let num = parseFloat(prompt("Ingrese un número: " ));
+    if (!isNaN(num)) {  
+        let factorial = 1;
+        for (let i = 1; i <= num; i++) {
+          factorial *= i;
+        }
+        console.log("El factorial es", factorial);
+    } else {
+        alert ("Error: Ingrese un número"); 
+    }
+};
+factorial();
